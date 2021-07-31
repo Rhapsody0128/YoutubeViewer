2a8951493d0efb7fe95b9da8657749ac8ea1c3fe
Rhapsody0128 <rhapsody0128@gmail.com>
2021-07-31 12:07:24 +0800

    設條件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,59 +4,64 @@
       Col(v-for="(card , index) in cards" :key='index' :xs="24" :sm="12" :md="12" :lg="8" :xl="6" :xxl="6")
         YoutubeCard(:data="card" :type='0')
 </template>
-
+<script src="https://apis.google.com/js/client.js?onload=onClientLoad"></script>
 <script>
 export default {
   data: function () {
     return {
       searchValue: '',
       cards: [],
-      videoDuration: 'medium'
+      videoDuration: 'medium',
+      dataReady: false
     }
   },
   methods: {
     recommend () {
-      const request = window.gapi.client.youtube.videos.list({
-        part: [
-          'snippet,contentDetails,statistics'
-        ],
-        regionCode: 'tw',
-        maxResults: 50,
-        chart: 'mostPopular'
-      })
-      var cards = []
-      request.execute(function (response) {
-        response.items.map(item => {
-          try {
-            if (!item.id.playlistId) {
-              var card = {}
-              card.id = item.id
-              card.title = item.snippet.title
-              card.url = 'https://www.youtube.com/embed/' + card.id
-              card.description = item.snippet.description
-              card.channelTitle = item.snippet.channelTitle
-              card.src = 'http://img.youtube.com/vi/' + card.id + '/0.jpg'
-              cards.push(card)
-              card.viewCount = item.statistics.viewCount
-            }
-          } catch (error) {
-            console.log(error)
-          }
+      if (this.dataReady === true) {
+        const request = window.gapi.client.youtube.videos.list({
+          part: [
+            'snippet,contentDetails,statistics'
+          ],
+          regionCode: 'tw',
+          maxResults: 50,
+          chart: 'mostPopular'
         })
-      })
-      this.cards = cards
+        var cards = []
+        request.execute(function (response) {
+          response.items.map(item => {
+            try {
+              if (!item.id.playlistId) {
+                var card = {}
+                card.id = item.id
+                card.title = item.snippet.title
+                card.url = 'https://www.youtube.com/embed/' + card.id
+                card.description = item.snippet.description
+                card.channelTitle = item.snippet.channelTitle
+                card.src = 'http://img.youtube.com/vi/' + card.id + '/0.jpg'
+                card.viewCount = item.statistics.viewCount
+                cards.splice(Math.floor(Math.random()*cards.length),0,card)
+              }
+            } catch (error) {
+              console.log(error)
+            }
+          })
+        })
+        this.cards = cards
+      }
     },
     setApi () {
       return new Promise(function (resolve, reject) {
         window.gapi.client.load('youtube', 'v3', resolve)
         window.gapi.client.setApiKey(process.env.VUE_APP_YOUTUBE_API)
       })
+      this.dataReady = true
     }
   },
   mounted: async function () {
     // await window.gapi.client.load('youtube', 'v3')
     // await window.gapi.client.setApiKey(process.env.VUE_APP_YOUTUBE_API)
     await this.setApi()
+    this.dataReady = true
     this.recommend()
   }
 }
