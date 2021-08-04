0ebcb0566a5d179768bb609202b5cd6674f72198
Rhapsody0128 <61406219+Rhapsody0128@users.noreply.github.com>
2021-08-04 12:38:49 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,8 @@
             Avatar(size="45" style="color: #f56a00;background-color: #fde3cf") {{name}}
           .googleInfo(v-else @click="login()")
             Avatar(icon="ios-person" size="45")
-    router-view
+    keep-alive
+      router-view
 </template>
 <script src="https://apis.google.com/js/api.js"></script>
 <script src="https://apis.google.com/js/client.js"></script>
